2bfe59b9f9e81c3f361b5a58c3951d7c14fbdf2d
MWB <brucemwb2018@gmail.com>
2024-05-11 16:28:00 +0800

    new mock  interface

--- Vue3Project/src/main.ts
+++ Vue3Project/src/main.ts
@@ -9,7 +9,17 @@ app.component('SvgIcon',SvgIcon);*/
 //第二种统一注册全局组件方法
 import globalComponent from './components'
 app.use(globalComponent)
-
+//引入模板全局样式
 import "./styles/index.scss"
+/*//测试mock接口
+import axios  from 'axios';
+axios({
+    url: '/api/user/login',
+    method: 'post',
+    data: {
+        username: 'admin',
+        password: '1233456',
+    }
+}).then(r  =>  '200')*/
 
 app.mount('#app')
